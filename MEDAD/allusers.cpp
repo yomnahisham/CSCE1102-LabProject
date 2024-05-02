6c4a7072f0cda595996582fd2b9351fa75438bf2
aylasaleh <aylasaleh@aucegypt.edu>
2024-05-02 20:53:02 +0300

    save Users fixed

--- MEDAD/allusers.cpp
+++ MEDAD/allusers.cpp
@@ -3,7 +3,11 @@
 #include "admin.h"
 #include "user.h"
 #include <QFile>
+#include <QDir>
+#include <QFileInfo>
 #include <QTextStream>
+#include <QStandardPaths>
+#include <QCoreApplication>
 
 int AllUsers::m = 17;
 
@@ -17,22 +21,55 @@ AllUsers& AllUsers::operator=( AllUsers& other) { // add const
     return *this;
 }
 
+QString cachedResource(const QString &resPath) {
+    // Not a resource -> done
+    if (!resPath.startsWith(":"))
+        return resPath;
+
+    // Get the cache directory of your app relative to the executable
+    QString executablePath = QCoreApplication::applicationDirPath();
+    QString cacheDir = executablePath + "/cache";
+
+    // Construct the path for the cached resource
+    QString subPath = cacheDir + resPath.mid(1); // cache folder plus resource without the leading ':'
+
+    // Check if the resource is already cached
+    if (QFile::exists(subPath)) // File exists -> done
+        return subPath;
+
+    // Ensure the cache directory exists
+    if (!QFileInfo(cacheDir).dir().mkpath("."))
+        return {}; // Failed to create dir
+
+    // Copy the resource file to the cache directory
+    if (!QFile::copy(resPath, subPath))
+        return {}; // Failed to copy file
+
+    // Make the copied file writable
+    QFile::setPermissions(subPath, QFileDevice::ReadUser | QFileDevice::WriteUser);
+
+    return subPath;
+}
+
 void AllUsers::LoadUsers(){
     //load all user data
-    QFile userData (":/UsersInfo/UserData.txt");
-    if (!userData.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Failed to open file:" << userData.errorString();
-        return;
-    }
+    QFile userData (cachedResource(":/UsersInfo/UserData.txt"));
+
+    if (userData.setPermissions(QFile::ReadOwner))
+        qDebug() << "Permissions updated successfully for file:";
+    else
+        qDebug() << "Error updating permissions for file:" ;
+
+    userData.open(QIODevice::ReadOnly | QIODevice::Text);
 
     QTextStream in(&userData);
 
     while(!in.atEnd()) {
         QString data = in.readLine();
+        qDebug()<< data;
         QStringList splitdata = data.split (" ");
 
         vector<QString> genres;
-
 
         if (splitdata[0] == "admin")
            insert (admin,splitdata[1], splitdata[2], genres);
@@ -45,13 +82,14 @@ void AllUsers::LoadUsers(){
     }
 
     userData.close();
-
     finishedloading = true;
 }
 
 void AllUsers::SaveUsers()
 {
-    QFile userData (":/UsersInfo/UserData.txt");
+    qDebug() << "Saving users";
+
+    QFile userData (cachedResource(":/UsersInfo/UserData.txt"));
 
     if (userData.setPermissions(QFile::WriteOwner)) {
         qDebug() << "Permissions updated successfully for file:";
@@ -59,26 +97,25 @@ void AllUsers::SaveUsers()
         qDebug() << "Error updating permissions for file:" ;
     }
 
-    userData.open(QIODevice::WriteOnly | QIODevice::Text);
-    if (userData.isOpen()) {
-        qDebug ()<< "opened file:";
+    userData.open(QIODevice::WriteOnly | QIODevice::Text|QIODevice::Truncate);
 
-        QTextStream stream(&userData);
+    QTextStream stream(&userData);
+    /*stream << "customer yomna yomna1 Classics Comic-Books N" << "\n";
+    stream << "customer aylaS ayla1234 Classics Palestine N" << "\n";
+    stream << "admin AylaSaleh ayla1234" << "\n";
+    stream << "admin yoyoo yomna1" << "\n";*/
 
-        for (int i = 0 ; i < m; i ++)
-        {
-            stream << "admin " << AllAdmins[i].getUsername() << " " << AllAdmins[i].getPassword() << "/n" ;
-            stream << "customer " << AllCustomers[i].getUsername() << " " << AllCustomers[i].getPassword() ;
-            vector <QString> genre = AllCustomers[i].getPreferredGenres();
-            for (auto it = genre.begin(); it != genre.end(); it++)
-                stream << " " <<*it;
-            stream << "/n";
-        }
+    for (int i = 0 ; i < m; i ++)
+    {
+        stream << "admin " << AllAdmins[i].getUsername() << " " << AllAdmins[i].getPassword() << "\n" ;
+        stream << "customer " << AllCustomers[i].getUsername() << " " << AllCustomers[i].getPassword() ;
+        vector <QString> genre = AllCustomers[i].getPreferredGenres();
+        for (auto it = genre.begin(); it != genre.end(); it++)
+            stream << " " <<*it;
+        stream << " N" << "\n";
+    }
 
-        userData.close();
-    } else
-        qDebug ()<< "Error opening file:" << userData.errorString();
-
+    userData.close();
 }
 
 int AllUsers:: hash (QString u, int att)
@@ -118,6 +155,8 @@ void AllUsers::insert (Type type, QString u, QString p, vector<QString> genres)
         }
         break;
     }
+
+    qDebug()<< finishedloading;
 
     if (finishedloading)
     {
